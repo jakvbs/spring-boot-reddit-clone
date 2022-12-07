f0899f82a26c93315676ce911db09152a3d92988
Jakub Sieczczyński <j.sieczczynski@gmail.com>
2022-12-07 08:56:34 +0100

    Add project boilerplate

---
+++ src/main/java/pl/jsieczczynski/SpringBootRedditClone/SpringBootRedditCloneApplication.java
@@ -0,0 +1,13 @@
+package pl.jsieczczynski.SpringBootRedditClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedditCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRedditCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jsieczczynski/SpringBootRedditClone/SpringBootRedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jsieczczynski.SpringBootRedditClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
